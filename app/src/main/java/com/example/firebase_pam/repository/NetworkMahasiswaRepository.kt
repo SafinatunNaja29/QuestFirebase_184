0deeb9f4ace08e49f4711a0c53d7db631e7ad5fb
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-01-12 21:21:53 +0700

    Menambahkan method untuk update data mahasiswa ke  Firestore

--- app/src/main/java/com/example/firebase_pam/repository/NetworkMahasiswaRepository.kt
+++ app/src/main/java/com/example/firebase_pam/repository/NetworkMahasiswaRepository.kt
@@ -40,7 +40,15 @@ class NetworkMahasiswaRepository (
     }
 
     override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
-
+        try {
+            firestore.collection("Mahasiswa")
+                .document(mahasiswa.nim)
+                .set(mahasiswa)
+                .await()
+        } catch (e: Exception) {
+            throw Exception ("Gagal mengupdate data mahasiswa: " +
+                    "${e.message}")
+        }
     }
 
     override suspend fun deleteMahasiswa(nim: String) {
