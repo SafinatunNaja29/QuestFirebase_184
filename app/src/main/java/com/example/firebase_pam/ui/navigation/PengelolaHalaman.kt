0aedc4c7f5f5168cbf2c69a93e7c4544b3330871
SafinatunNaja29 <safinatun.nj29@gmail.com>
2025-01-12 22:24:30 +0700

    Menambahkan fungsi PengelolaHalaman

--- app/src/main/java/com/example/firebase_pam/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/firebase_pam/ui/navigation/PengelolaHalaman.kt
@@ -1,2 +1,39 @@
 package com.example.firebase_pam.ui.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.firebase_pam.ui.view.HomeScreen
+import com.example.firebase_pam.ui.view.InsertView
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToltemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+
+        composable(DestinasiInsert.route) {
+            InsertView(
+                onBack = { navController.popBackStack() },
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
